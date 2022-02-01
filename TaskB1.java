7274a69327c971d8706f982d76f588d105d253f7
Melody Duong <melodyduong08@gmail.com>
2022-01-31 18:08:57 -0800

    Added comments

--- TaskB1.java
+++ TaskB1.java
@@ -5,7 +5,10 @@ Assignment 6
 1 December 2021
 */
 import java.util.Scanner;
-
+/**
+ * This class creates a program that calculates the sum of all integers
+ * up to a specified integer.
+ */
 public class TaskB1 {
     public static void main(String[] args)
     {
@@ -19,7 +22,12 @@ public class TaskB1 {
         total = sum(number);
         System.out.println(total);
     }
-
+/**
+ * The sum method takes in the number from the user and returns the sum using a 
+ * simple recursive program.
+ * @param input the integer argument passed from the main method
+ * @return returns the sum of the integers
+ */
     public static int sum(int input)
     {
         if (input == 1)
